0a8fa3e2e9333151c9b37e82c4046b4bc5d1c3d0
Justin <justin.chau623@gmail.com>
2016-12-29 00:19:18 -0800

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,13 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+// import App from './App' //(demo page)
+import '../node_modules/semantic-ui-css/semantic.min.css'
+import semantic from 'semantic'
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello!'
+  }
+})
